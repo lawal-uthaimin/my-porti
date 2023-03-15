16b8b6c56c5094777b0c99065a7339bc85ee1dc0
lawal uthaimin <lawaluthaimin06@gmail.com>
2023-03-16 00:27:21 +0100

    setting up front end

---
+++ HTML/drake/assets/js/ajax-form.js
@@ -0,0 +1,28 @@
+// const message=document.getElementById('message')
+// const fullName=document.getElementById("full-name")
+// const email=document.getElementById("email")
+// const subject=document.getElementById("subject")
+// const uploadAttach= document.getElementById("upload-attachment")
+// const form = document.querySelector(".contact-form")
+// console.log(form)
+
+// form.addEventListener('submit', (e) =>{
+//     e.preventDefault()
+//     console.log("çliked")
+
+//     Email.send({
+//         SecureToken : "b53339e5-4a3e-4195-8b73-e63046955011",
+//         To : 'lawaluthaimin06@gmail.com',
+//         From : email.value,
+//         Subject : subject.value,
+//         Body : message.value
+//     }).then(
+//         message => alert(message)
+//     );
+
+// })
+
+     
+
+  
+
---
+++ HTML/drake/assets/js/move.js
@@ -0,0 +1,16 @@
+// const premium = document.querySelector('.premium')
+// const forrm = document.querySelector(".contact-form")
+// console.log(premium)
+// const subjectt=document.getElementById("subject")
+// premium.addEventListener('click', (e) =>{
+//     e.preventDefault()
+//     if(premium){
+//         console.log('hello')
+//         scrollTo(forrm)
+//         subjectt.textContent = 'premium selected'
+//         console.log(subjectt.innerText)
+//     }
+// })
+// // getting 
+
+// // console.log('hello')
